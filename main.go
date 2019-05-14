e7b27355045ee88ba2fdc11b890c96c140abb488
Williamrobert Green <wkgreen13@gmail.com>
2019-05-14 15:41:39 -0400

    adds comments

--- main.go
+++ main.go
@@ -7,23 +7,30 @@ import (
 )
 
 func main() {
+	// SET PORT
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "4000"
 	}
+
+	// GET MULTIPLEX
 	mux := createMux()
+
+	// START SERVER
 	fmt.Println("Server listening on", port)
 	if error := http.ListenAndServe(":"+port, mux); error != nil {
 		fmt.Println("ERROR", error)
 	}
 }
 
+// CREATE MULTIPLEX PATH HANDLER
 func createMux() (mux *http.ServeMux) {
 	mux = http.NewServeMux()
 	mux.HandleFunc("/", root)
 	return
 }
 
+// ROOT GETTER
 func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Welcome to the Dungeon")
 }
