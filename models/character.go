ae48aeed3fee8dc06eac9d85e6e9b6475a34abb2
Williamrobert Green <wkgreen13@gmail.com>
2019-05-22 14:20:02 -0400

    ignores ID in firestore

--- models/character.go
+++ models/character.go
@@ -1,25 +1,25 @@
 package models
 
 type Character struct {
-	ID    string `json:"id"`
+	ID    string `json:"id" firestore:"-"`
 	Name  string `json:"name"`
 	Race  string `json:"race"`
 	Class string `json:"class"`
 	XP    int    `json:"xp"`
-	Level int    `json:"level"`
+	HP    int    `json:"hp"`
+	Armor int    `json:"armor"`
+	Speed int    `json:"speed"`
 	Str   int    `json:"str"`
 	Dex   int    `json:"dex"`
 	Con   int    `json:"con"`
 	Intel int    `json:"intel"`
 	Wis   int    `json:"wis"`
 	Cha   int    `json:"cha"`
-	HP    int    `json:"hp"`
-	Armor int    `json:"armor"`
-	Speed int    `json:"speed"`
 }
 
 type Level struct {
-	XP               int
-	Level            int
-	ProficiencyBonus int
+	MinXP  int `json:"-"`
+	Level  int `json:"level"`
+	Bonus  int `json:"proBonus"`
+	NextXP int `json:"next"`
 }
