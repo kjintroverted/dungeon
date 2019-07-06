94f75678bdabc17a2e2cbe2d52897c035189b323
Williamrobert Green <wkgreen13@gmail.com>
2019-07-06 16:33:52 -0400

    adds auth user endpoints

--- models/character.go
+++ models/character.go
@@ -1,19 +1,20 @@
 package models
 
 type Character struct {
-	ID    string `json:"id" firestore:"-"`
-	Owner string `json:"owner"`
-	Name  string `json:"name"`
-	Race  string `json:"race"`
-	Class string `json:"class"`
-	XP    int    `json:"xp"`
-	HP    int    `json:"hp"`
-	Armor int    `json:"armor"`
-	Speed int    `json:"speed"`
-	Str   int    `json:"str"`
-	Dex   int    `json:"dex"`
-	Con   int    `json:"con"`
-	Intel int    `json:"intel"`
-	Wis   int    `json:"wis"`
-	Cha   int    `json:"cha"`
+	ID              string   `json:"id" firestore:"-"`
+	Owner           string   `json:"owner"`
+	AuthorizedUsers []string `json:"-"`
+	Name            string   `json:"name"`
+	Race            string   `json:"race"`
+	Class           string   `json:"class"`
+	XP              int      `json:"xp"`
+	HP              int      `json:"hp"`
+	Armor           int      `json:"armor"`
+	Speed           int      `json:"speed"`
+	Str             int      `json:"str"`
+	Dex             int      `json:"dex"`
+	Con             int      `json:"con"`
+	Intel           int      `json:"intel"`
+	Wis             int      `json:"wis"`
+	Cha             int      `json:"cha"`
 }
