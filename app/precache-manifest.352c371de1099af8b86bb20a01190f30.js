577fee45eae9ec183896f7bb63a57b520b210cc4
Williamrobert Green <wkgreen13@gmail.com>
2019-08-06 15:48:36 -0400

    adds built app to server

---
+++ app/precache-manifest.352c371de1099af8b86bb20a01190f30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b93d290bde6bf0ebde11efa5fa18735c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c12317d4cd446db0c35",
+    "url": "/static/css/main.ad91e56c.chunk.css"
+  },
+  {
+    "revision": "b79420a45899f483b29b",
+    "url": "/static/js/2.b4285749.chunk.js"
+  },
+  {
+    "revision": "4c12317d4cd446db0c35",
+    "url": "/static/js/main.4bec56dc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ app/precache-manifest.7a8c514407980099faedc05aabd3270d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c138e28997588e572c79a27d01b97c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2926188dcef97b7348db",
+    "url": "/static/css/main.ad91e56c.chunk.css"
+  },
+  {
+    "revision": "426c8a4097dd3e9f3906",
+    "url": "/static/js/2.0161d6cf.chunk.js"
+  },
+  {
+    "revision": "2926188dcef97b7348db",
+    "url": "/static/js/main.39a17f03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
