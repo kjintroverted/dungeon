f4a87c402a6caa147bd3bffefe31a06522c86e67
Williamrobert Green <wkgreen13@gmail.com>
2019-12-20 09:17:28 -0500

    adds qty to inventory

--- models/item.go
+++ models/item.go
@@ -1,7 +1,8 @@
 package models
 
 type Item struct {
-	Name     string `json:"name"`
-	Desc     string `json:"description"`
-	GoldCost float64    `json:"goldCost"`
+	Name     string  `json:"name"`
+	Qty      int     `json:"qty"`
+	Desc     string  `json:"description"`
+	GoldCost float64 `json:"goldCost"`
 }
