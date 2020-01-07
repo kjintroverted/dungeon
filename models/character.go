9522be9b49f3fa2a473dbe435fdd941ef2d7295c
Williamrobert Green <wkgreen13@gmail.com>
2020-01-07 10:17:33 -0500

    wip

--- models/character.go
+++ models/character.go
@@ -3,37 +3,37 @@ package models
 import "github.com/kjintroverted/dungeon/util"
 
 type Character struct {
-	ID              string   `json:"id" firestore:"-"`
-	Owner           string   `json:"owner"`
-	AuthorizedUsers []string `json:"authUsers"`
-	ReadUsers       []string `json:"visibleTo"`
-	Name            string   `json:"name"`
-	Race            string   `json:"race"`
-	Class           string   `json:"class"`
-	Alignment       string   `json:"alignment"`
-	XP              int      `json:"xp"`
-	Level           int      `json:"level" firestore:"-"`
-	ProBonus        int      `json:"proBonus" firestore:"-"`
-	NextXP          int      `json:"next" firestore:"-"`
-	HP              int      `json:"hp"`
-	MaxHP           int      `json:"maxHP"`
-	Armor           int      `json:"armor"`
-	Speed           int      `json:"speed"`
-	Str             int      `json:"str"`
-	Dex             int      `json:"dex"`
-	Con             int      `json:"con"`
-	Intel           int      `json:"int"`
-	Wis             int      `json:"wis"`
-	Cha             int      `json:"cha"`
-	Initiative      int      `json:"initiative,omitempty"`
-	ProSkills       []string `json:"proSkills"`
-	ProTools        []string `json:"proTools"`
-	Languages       []string `json:"languages"`
-	Weapons         []Weapon `json:"weapons"`
-	Inventory       []Item   `json:"inventory"`
-	GP              float64  `json:"gold"`
-	Spells          []string `json:"spells,omitempty"`
-	Features        []int    `json:"feats,omitempty"`
+	ID              string    `json:"id" firestore:"-"`
+	Owner           string    `json:"owner"`
+	AuthorizedUsers []string  `json:"authUsers"`
+	ReadUsers       []string  `json:"visibleTo"`
+	Name            string    `json:"name"`
+	Race            string    `json:"race"`
+	Class           string    `json:"class"`
+	Alignment       string    `json:"alignment"`
+	XP              int       `json:"xp"`
+	Level           int       `json:"level" firestore:"-"`
+	ProBonus        int       `json:"proBonus" firestore:"-"`
+	NextXP          int       `json:"next" firestore:"-"`
+	HP              int       `json:"hp"`
+	MaxHP           int       `json:"maxHP"`
+	Armor           int       `json:"armor"`
+	Speed           int       `json:"speed"`
+	Str             int       `json:"str"`
+	Dex             int       `json:"dex"`
+	Con             int       `json:"con"`
+	Intel           int       `json:"int"`
+	Wis             int       `json:"wis"`
+	Cha             int       `json:"cha"`
+	Initiative      int       `json:"initiative,omitempty"`
+	ProSkills       []string  `json:"proSkills"`
+	ProTools        []string  `json:"proTools"`
+	Languages       []string  `json:"languages"`
+	Weapons         []Weapon  `json:"weapons"`
+	Inventory       []Item    `json:"inventory"`
+	GP              float64   `json:"gold"`
+	Spells          []string  `json:"spells,omitempty"`
+	Features        []Feature `json:"feats,omitempty"`
 }
 
 func (c *Character) PopulateLevelInfo() {
--- models/feat.go
+++ models/feat.go
@@ -1,6 +1,7 @@
 package models
 
 type Feature struct {
+	Index string   `json:"index"`
 	Name  string   `json:"name"`
 	Level int      `json:"level,omitempty"`
 	URL   string   `json:"url"`
